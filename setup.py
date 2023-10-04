36dbc9024e7d0705ed73e509d0e4dec0522b5ab7
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-10-04 01:19:59 -0400

    releasing 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-advanced-typing",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     install_requires=[],
     author="Yidi Sprei",
