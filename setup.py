e067b11a728947b267d6d5ef9f82082e9194f6dd
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-10-04 01:57:30 -0400

    troubleshooting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-advanced-typing",
-    version="0.12",
+    version="0.13",
     packages=find_packages(),
     install_requires=[],
     author="Yidi Sprei",
